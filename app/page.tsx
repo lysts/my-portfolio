4ef31aaf85412b9726af500ac8a9a3b919b33755
Lyla <123667606+lysts@users.noreply.github.com>
2024-03-28 17:09:26 +0000

    new portfolio basic set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Intro from "@/components/intro";
-import SectionDivider from "@/components/section-divider";
-import About from "@/components/about"
-import Projects from "@/components/projects"
-import Skills from "@/components/skills"
-import Experience from "@/components/experience"
+import About from "@/components/about";
 import Contact from "@/components/contact";
+import Experience from "@/components/experience";
+import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -17,5 +17,5 @@ export default function Home() {
       <Experience />
       <Contact />
     </main>
-  )
+  );
 }
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,21 +1,74 @@
-import React from 'react'
-import { FaPaperPlane } from 'react-icons/fa'
-import SectionHeading from './section-heading'
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
 export default function Contact() {
-  return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
-        <SectionHeading>Contact me</SectionHeading>
-        <p className="text-gray-700 -mt-6">Please contact me directly at{" "}<a className="underline" href="mailto:lyla.taguma@gmail.com">lyla.taguma@gmail.com</a>{" "}or through this form.</p>
+  const { ref } = useSectionInView("Contact");
 
-        <form className="mt-10 flex flex-col">
-            <input type="email" className="h-14 px-4 rounded-lg borderBlack" placeholder="Your email"></input>
-            <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Your message"/>
-            <button type="submit" className="group flex items-center justify-center gap-2 h-[3-rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:bg-gray-950 hover:scale-110 active:scale-105">
-              Submit <FaPaperPlane className="text-cs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>{" "}
-            </button>
-        
-        </form>
-    </section>
-  )
+  return (
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:example@gmail.com">
+          example@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
+
+      <form
+        className="mt-10 flex flex-col dark:text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
+    </motion.section>
+  );
 }
