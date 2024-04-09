f653d72f099174e69168e63cc489890e4cf759dc
Lyla <123667606+lysts@users.noreply.github.com>
2024-04-09 16:31:26 +0100

    contacts section email

--- components/contact.tsx
+++ components/contact.tsx
@@ -29,12 +29,12 @@ export default function Contact() {
         once: true,
       }}
     >
-      <SectionHeading>Contact me</SectionHeading>
+      <SectionHeading>Got an exciting idea? Contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="mailto:lyla.taguma@gmail.com">
+          lyla.taguma@gmail.com
         </a>{" "}
         or through this form.
       </p>
