b7d97b23411f3a7172bed6c0beb2b50fdeeda119
Lyla <123667606+lysts@users.noreply.github.com>
2024-04-24 22:08:11 +0100

    fix link/addcv

--- components/intro.tsx
+++ components/intro.tsx
@@ -107,7 +107,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com/in/lyla-taguma-0503361b0"
+          href="https://www.linkedin.com/in/lyla-taguma-0503361b0"
           target="_blank"
         >
           <BsLinkedin />
