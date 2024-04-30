abfec77b1312b8c5f86ba3863eefa3b9f86295e5
Lyla <123667606+lysts@users.noreply.github.com>
2024-04-30 18:38:01 +0100

    fixed lowercase

--- lib/types.ts
+++ lib/types.ts
@@ -4,4 +4,4 @@ export type SectionName = (typeof links)[number]["name"];
 
 import { mainLinks } from "./data";
 
-export type NavName = (typeof mainlinks)[number]["name"];
+export type NavName = (typeof mainLinks)[number]["name"];
